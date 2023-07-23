95a96ac34d5ae302bee4d3b987db138bad90207c
Hyeonseok Jung <hsjames0248@gmail.com>
2023-07-23 18:42:04 +0900

    Add dependency and reproduce error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
+import { PreTrainedTokenizer } from '@xenova/transformers'
 
 export default function Home() {
+  const tokenizer = new PreTrainedTokenizer({}, {})
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
